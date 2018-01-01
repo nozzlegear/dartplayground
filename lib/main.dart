2d29fe019c20f92a38524f7b356ab71b958d9672
Joshua Harms <joshua@nozzlegear.com>
2017-12-31 23:00:40 -0600

    Really struggling to make JSON work but sadly it's very difficult even with packages like dson or json_serializable 😓

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-@MirrorsUsed()
+library sci_tally_tool;
+
 import "dart:async";
 import "dart:io" show Platform;
-import "dart:mirrors";
-import "dart:convert" show UTF8, BASE64, Encoding;
+import "dart:convert" show UTF8, BASE64, Encoding, JSON;
 import "package:logging/logging.dart";
 import "package:intl/intl.dart" show DateFormat;
-import 'package:json_god/json_god.dart' as god;
 import 'package:http/http.dart' as http;
 
 final Logger log = new Logger("main");
@@ -39,8 +38,10 @@ String buildApiUrl(DateTime startDate, DateTime endDate) {
   return "$protocol://$apiDomain/api/v1/orders/tally/sources?since=${startDate.millisecondsSinceEpoch}&until=${endDate.millisecondsSinceEpoch}";
 }
 
-Future<http.Response> makeRequest<T>(Uri url, String method,
-    {T body = null, Map<String, String> customHeaders = null}) async {
+Future<http.Response> makeRequest(Uri url, String method,
+    {String body = null, Map<String, String> customHeaders = null}) async {
+  customHeaders ??= {};
+
   Map<String, String> headers = new Map.from(customHeaders)
     ..putIfAbsent("Content-Type", () => "application/json")
     ..putIfAbsent("Accept", () => "application/json");
@@ -48,43 +49,21 @@ Future<http.Response> makeRequest<T>(Uri url, String method,
 
   switch (method.toLowerCase()) {
     case "post":
-      resp = await http.post(url,
-          body: god.serialize(body),
-          encoding: Encoding.getByName("UTF8"),
-          headers: headers);
+      resp = await http.post(url, body: body, encoding: Encoding.getByName("UTF8"), headers: headers);
       break;
     case "get":
       resp = await http.get(url, headers: headers);
       break;
     default:
-      throw new UnimplementedError(
-          "Attempted to create HttpClient with unsupported HTTP method $method.");
+      throw new UnimplementedError("Attempted to create HttpClient with unsupported HTTP method $method.");
   }
 
   return resp;
-
-  // req
-  //   ..headers.contentType = ContentType.JSON
-  //   ..headers.add("Accept", ContentType.JSON);
-
-  // if (headers != null) {
-  //   headers.forEach((key, value) => req.headers.set(key, value));
-  // }
-
-  // if (body != null) {
-  //   String json = god.serialize(body);
-  //   log.info(json);
-  //   req.write(json);
-  //   await req.flush();
-  // }
-
-  // return await req.close();
 }
 
 void ensureSuccessResponse(http.Response resp) {
   if (resp.statusCode < 200 || resp.statusCode >= 300) {
-    String message =
-        "Request to ${resp.request.url} failed with ${resp.statusCode} ${resp.reasonPhrase}.";
+    String message = "Request to ${resp.request.url} failed with ${resp.statusCode} ${resp.reasonPhrase}.";
 
     log.severe("$message Response body: ${resp.body}");
 
@@ -92,41 +71,32 @@ void ensureSuccessResponse(http.Response resp) {
   }
 }
 
-Future<T> makeGetRequest<T>(String url,
-    [Map<String, String> headers = null]) async {
-  var resp =
-      await makeRequest<Object>(Uri.parse(url), "GET", customHeaders: headers);
+Future<T> makeGetRequest<T>(String url, [Map<String, String> headers = null]) async {
+  var resp = await makeRequest(Uri.parse(url), "GET", customHeaders: headers);
 
   ensureSuccessResponse(resp);
 
-  return god.deserialize(resp.body) as T;
+  return JSON.decode(resp.body);
 }
 
-Future<T> makePostRequest<D, T>(String url, D body,
-    [Map<String, String> headers = null]) async {
-  http.Response resp = await makeRequest(Uri.parse(url), "POST",
-      body: body, customHeaders: headers);
+Future<T> makePostRequest<T>(String url, String body, [Map<String, String> headers = null]) async {
+  http.Response resp = await makeRequest(Uri.parse(url), "POST", body: body, customHeaders: headers);
 
   ensureSuccessResponse(resp);
 
-  return god.deserialize(resp.body) as T;
+  return JSON.decode(resp.body);
 }
 
 SwuMessage buildEmailData(DateTime startDate, List<TallyTemplate> tally) {
   final emailDomain = envVarRequired("SCI_TALLY_EMAIL_DOMAIN");
-  final bool isLive =
-      envVarDefault("SCI_TALLY_ENV", "development") == "production";
+  final bool isLive = envVarDefault("SCI_TALLY_ENV", "development") == "production";
   final swuTemplateId = envVarRequired("SCI_TALLY_SWU_TEMPLATE_ID");
   final formatEmail = (String name) => "$name@$emailDomain";
-  final SwuRecipient emailRecipient = isLive
-      ? new SwuRecipient("Mike", formatEmail("mikef"))
-      : new SwuRecipient("Joshua Harms", formatEmail("josh"));
-  final List<SwuRecipient> ccs = isLive
-      ? god.deserialize(envVarRequired("SCI_TALLY_CC_LIST"))
-          as List<SwuRecipient>
-      : [];
-  final sender = new SwuSender("KMSignalR Superintendent",
-      formatEmail("superintendent"), formatEmail("superintendent"));
+  final SwuRecipient emailRecipient =
+      isLive ? new SwuRecipient("Mike", formatEmail("mikef")) : new SwuRecipient("Joshua Harms", formatEmail("josh"));
+  final List<SwuRecipient> ccs = isLive ? JSON.decode(envVarRequired("SCI_TALLY_CC_LIST")) : [];
+  final sender =
+      new SwuSender("KMSignalR Superintendent", formatEmail("superintendent"), formatEmail("superintendent"));
 
   return new SwuMessage()
     ..template = swuTemplateId
@@ -159,8 +129,11 @@ Future main(List<String> args) async {
   final headers = {
     "Authorization": "Basic ${BASE64.encode(UTF8.encode("$swuKey:"))}",
   };
-  Map<String, Object> emailResult = await makePostRequest(
-      "https://api.sendwithus.com/api/v1/send", emailMessage, headers);
+
+  print(JSON.encode(emailMessage));
+
+  Map<String, Object> emailResult =
+      await makePostRequest("https://api.sendwithus.com/api/v1/send", JSON.encode(emailMessage), headers);
 
   log.info("Email send result: $emailResult");
 }
@@ -168,23 +141,45 @@ Future main(List<String> args) async {
 class TallyTemplate {
   String source;
   int count;
+
   TallyTemplate(this.source, this.count);
+
+  Map toJson() {
+    return {"source": source, "count": count};
+  }
 }
 
 class SwuRecipient {
   String name;
   String address;
+
   SwuRecipient(this.name, this.address);
+
+  Map toJson() {
+    return {"name": name, "address": address};
+  }
 }
 
 class SwuSender extends SwuRecipient {
   String replyTo;
+
   SwuSender(String name, String address, this.replyTo) : super(name, address);
+
+  Map toJson() {
+    return super.toJson()..["replyTo"] = replyTo;
+  }
 }
 
 class SwuTallyTemplateData {
   String date;
   List<TallyTemplate> tally;
+
+  SwuTallyTemplateData();
+
+  Map toJson() {
+    tally ??= [];
+    return {"date": date, "tally": tally.map((t) => t.toJson())};
+  }
 }
 
 class SwuMessage {
@@ -193,4 +188,18 @@ class SwuMessage {
   List<SwuRecipient> cc;
   SwuSender sender;
   SwuTallyTemplateData template_data;
+
+  SwuMessage();
+
+  Map toJson() {
+    cc ??= [];
+
+    return {
+      "template": template,
+      "recipient": recipient?.toJson() ?? {},
+      "cc": cc.map((r) => r.toJson()) ?? {},
+      "sender": sender?.toJson() ?? {},
+      "template_data": template_data?.toJson() ?? {}
+    };
+  }
 }
